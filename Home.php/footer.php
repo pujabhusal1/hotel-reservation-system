8eadb88a0c39136e41bb1f065785897c4db2cb84
pujabhusal1 <pujabhusal1234@gmail.com>
2023-07-08 20:40:17 +0545

    Home of the websites

---
+++ Home.php/footer.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+    }
+
+    .footer {
+      text-align: center;
+      margin-top: 24px;
+      padding: 30px;
+      background-color: darkgreen;
+      font-size: 24px;
+      color: white;
+    }
+
+    .footer-icons {
+      margin-top: 10px;
+    }
+
+    .footer-icons a {
+      color: white;
+      margin-right: 10px;
+      text-decoration: none;
+      font-size: 20px;
+    }
+  </style>
+</head>
+<body>
+  <div class="footer">
+    <p>&copy; 2023 NEW NEW Resorts. All rights reserved.</p>
+    <div class="footer-icons">
+      <a href="#"><i class="fab fa-facebook"></i></a>
+      <a href="#"><i class="fab fa-twitter"></i></a>
+      <a href="#"><i class="fab fa-instagram"></i></a>
+      <a href="#"><i class="far fa-envelope"></i></a>
+    </div>
+  </div>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js"></script>
+</body>
+</html>
---
+++ Home.php/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+<div class="navbar">
+  <h1 class="title">NEW NEW RESOTS</h1>
+  <a href="#">Home</a>
+  <a href="#">About</a>
+  <a href="#">Services</a>
+  <a href="#">Contact</a>
+  <a href="#">Contact</a>
+  <a href="#">Contact</a>
+</div>
+
+<div>
+  <img src="images/cootage.jpg" alt="Image 1"> 
+</div>
+<br><br<br>
+<br<br><br><br><br>
+ 
+  <div class="container">
+    <div class="text-column">
+      <h1 class="centered-text">INTRODUCTION</h1>
+      <p class="resort-description">Experience luxury and tranquility at our exquisite resort located amidst breathtaking natural beauty. With a range of amenities and services tailored to your comfort, we guarantee a memorable stay.</p>
+      <p class="resort-description">Indulge in our world-class dining options, rejuvenate at our spa and wellness center, or embark on exciting adventures offered by our recreational activities. Our dedicated staff is committed to providing you with unparalleled hospitality throughout your stay.</p>
+      <p class="resort-description">Explore our website to discover more about our resort and make reservations for an unforgettable experience. We look forward to welcoming you to NEW NEW Resorts!</p>
+    </div>
+    <div class="image-column">
+      <img src="images/elephant.jpg" alt="Image 1">
+    </div>
+  </div>
+  <?php include('footer.php');
+ ?>
+</body>
+</html>
+
+
