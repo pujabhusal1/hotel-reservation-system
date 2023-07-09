e4e2575f69a72699042ac2a8c498b0473462860b
pujabhusal1 <pujabhusal1234@gmail.com>
2023-07-09 10:33:31 +0545

    Front

---
+++ Home.php/aboutus.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WELCOME TO NEW VIEW RESORTS</title>
+</head>
+<body>
+<div class="about-us">
+  <h2>About Us</h2>
+  <p>Welcome to our hotel! We are dedicated to providing exceptional service and creating memorable experiences for our guests. With our luxurious accommodations, exquisite dining options, and top-notch amenities, we strive to make your stay truly unforgettable.</p>
+  <p>Our dedicated team of professionals is committed to ensuring your comfort and satisfaction throughout your visit. Whether you are here for business or leisure, we aim to exceed your expectations and make you feel right at home.</p>
+  <p>Experience the epitome of hospitality at our hotel. We look forward to serving you and making your stay with us an extraordinary one.</p>
+</div>
+
+    
+</body>
+</html>
--- Home.php/index.php
+++ Home.php/index.php
@@ -12,10 +12,11 @@
 <div class="navbar">
   <h1 class="title">NEW NEW RESOTS</h1>
   <a href="#">Home</a>
-  <a href="#">About</a>
+  <a href="aboutus.php">Aboutus</a>
   <a href="#">Services</a>
-  <a href="#">Contact</a>
-  <a href="#">Contact</a>
+  <a href="#">Booking</a>
+  <a href="#">Signup</a>
+  <a href="#">Login</a>
   <a href="#">Contact</a>
 </div>
 
@@ -36,6 +37,57 @@
       <img src="images/elephant.jpg" alt="Image 1">
     </div>
   </div>
+  <style>
+  .grid-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  
+  .grid-item {
+    margin-bottom: 20px; /* Adjust the margin as needed */
+  }
+</style>
+
+<div class="grid-container">
+  <div class="grid-item">
+    <img src="images/1.jpg" alt="Image 1">
+    <p>Item 1</p>
+    <a href="book.php" class="book-now-button">Book Now</a>
+  </div>
+  <div class="grid-item">
+    <img src="images/2.jpg" alt="Image 2">
+    <p>Item 2</p>
+    <a href="book.php" class="book-now-button">Book Now</a>
+  </div>
+  <div class="grid-item">
+    <img src="images/3.jpg" alt="Image 3">
+    <p>Item 3</p>
+    <a href="book.php" class="book-now-button">Book Now</a>
+  </div>
+  <div class="grid-item">
+    <img src="images/4.jpg" alt="Image 4">
+    <p>Item 4</p>
+    <a href="book.php" class="book-now-button">Book Now</a>
+  </div>
+  <div class="grid-item">
+    <img src="images/5.jpg" alt="Image 5">
+    <p>Item 5</p>
+    <a href="book.php" class="book-now-button">Book Now</a>
+  </div>
+  <div class="grid-item">
+    <img src="images/6.jpg" alt="Image 6">
+    <p>Item 6</p>
+    <a href="book.php" class="book-now-button">Book Now</a>
+  </div>
+</div>
+
+</div>
+
+
+</div>
+
+
   <?php include('footer.php');
  ?>
 </body>
