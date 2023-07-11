b31ff914690f7df39d655bab4b461eedba761b6e
pujabhusal1 <pujabhusal1234@gmail.com>
2023-07-11 09:53:26 +0545

    add table css

--- home/allbooking/allbooking.php
+++ home/allbooking/allbooking.php
@@ -1,57 +1,55 @@
-
 <?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "records";
 
+$conn = mysqli_connect($host, $user, $pass, $dbname);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
-    $host = "localhost";
-    $user = "root";
-    $pass = "";
-    $dbname = "records";
+$sql = "SELECT * FROM book";
 
-    $conn = mysqli_connect($host, $user, $pass, $dbname);
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
-
-    $sql ="Select * from book";
-    
-    if (mysqli_query($conn, $sql)) {
-        $result = mysqli_query($conn, $sql);
-        if (mysqli_num_rows($result) > 0) {
-            // Start building the HTML table
-            $html = "<table>";
-            
-            // Create table headers using the column names
-            $html .= "<tr>";
-            while ($field = mysqli_fetch_field($result)) {
-                $html .= "<th>" . $field->name . "</th>";
+if (mysqli_query($conn, $sql)) {
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        // Start building the HTML table
+        $html = "<table style='border-collapse: collapse; width: 100%;'>";
+       
+        // Create table headers using the column names
+        $html .= "<tr style='background-color: #006400;'>";
+        while ($field = mysqli_fetch_field($result)) {
+            $html .= "<th style='border: 1px solid #ddd; padding: 8px; background-color: #4287f5; color: #fff;'>" . $field->name . "</th>";
+        }
+        $html .= "</tr>";
+       
+        // Fetch and add data rows to the table
+        $rowNum = 0;
+        while ($row = mysqli_fetch_assoc($result)) {
+            $html .= "<tr style='background-color: " . ($rowNum % 2 == 0 ? "#f9f9f9" : "#ffffff") . ";'>";
+            foreach ($row as $value) {
+                $html .= "<td style='border: 1px solid #ddd; padding: 8px; color: #333;'>" . $value . "</td>";
             }
             $html .= "</tr>";
-            
-            // Fetch and add data rows to the table
-            while ($row = mysqli_fetch_assoc($result)) {
-                $html .= "<tr>";
-                foreach ($row as $value) {
-                    $html .= "<td>" . $value . "</td>";
-                }
-                $html .= "</tr>";
-            }
-            
-            // Close the table
-            $html .= "</table>";
-            
-            // Output or store the generated HTML table
-            echo $html;
-        } else {
-            echo "No rows found.";
+            $rowNum++;
         }
-        
-        // Free the result set
-        mysqli_free_result($result);
+       
+        // Close the table
+        $html .= "</table>";
+       
+        // Output or store the generated HTML table
+        echo $html;
     } else {
-        echo "Error Login failed" . mysqli_error($conn);
+        echo "No rows found.";
     }
+   
+    // Free the result set
+    mysqli_free_result($result);
+} else {
+    echo "Error: " . mysqli_error($conn);
+}
+
 mysqli_close($conn);
 exit(); // Stop executing the rest of the code
-
-
 ?>
